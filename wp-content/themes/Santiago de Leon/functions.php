446b94946da798bd47da610ed3d46cabdd4818f8
AndreSalazar96 <asalazar@analiticom.com>
2019-05-17 12:09:34 -0400

    editHomeAndAdded404Page

--- wp-content/themes/Santiago de Leon/functions.php
+++ wp-content/themes/Santiago de Leon/functions.php
@@ -11,3 +11,5 @@ function enqueue_child_theme_style() {
 function enqueue_child_theme_scripts() {
  wp_enqueue_script( 'child-js', get_stylesheet_directory_uri() . '/assets/js/main.js');
 }add_action('wp_enqueue_scripts','enqueue_child_theme_scripts');
+
+wp_enqueue_script( 'jquery', get_template_directory_uri() . '/assets/js/jquery.js', array('jquery'), '1.0', true );
