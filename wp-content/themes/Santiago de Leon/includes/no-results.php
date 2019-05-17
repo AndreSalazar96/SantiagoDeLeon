5e1111332a3c76d0cfe4737653e57218df66e3f4
AndreSalazar96 <asalazar@analiticom.com>
2019-05-17 14:27:57 -0400

    addIncludesFiles

--- wp-content/themes/Santiago de Leon/includes/no-results.php
+++ wp-content/themes/Santiago de Leon/includes/no-results.php
@@ -2,6 +2,6 @@
 <!--If no results are found-->
 	<h1 class="textNumber404"><?php esc_html_e('404','Divi'); ?></h1>
 	<p class="text-after-404-number"><?php esc_html_e("Parece que estás un poco perdido, no importa, vuelve con nosotros dando click en la casita.",'Divi'); ?></p>
-	<a href="/SantiagoDeLeon"><button class="comeback404"><img src="http://localhost/SantiagoDeLeon/wp-content/uploads/2019/05/hospital-1.png" alt="" class="home-icon-404"></button></a>
+	<a href="/santiagodeleon"><button class="comeback404"><img src="http://dev-analiticom.com/santiagodeleon/wp-content/uploads/2019/05/hospital-1.png" alt="" class="home-icon-404"></button></a>
 </div>
 <!--End if no results are found-->
